33523f18e484812e294e2c0264c129e0703bceb0
lavanya <lavanyathimmareddy20@gmail.com>
2024-08-27 12:47:48 +0530

    update

---
+++ day1/main.tf
@@ -0,0 +1,5 @@
+
+resource "aws_s3_bucket" "s3" {
+    bucket = var.aws_s3_bucket
+  
+}
---
+++ day1/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_s3_bucket" {
+    description = "passing the value to main file"
+    type = string
+    default = ""
+  
+}
